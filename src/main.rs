9a9aaaf0539b24bceb56f41108a69f048169f922
Danilo A. Silva <aotokobayashi@pop-os.localdomain>
2023-09-25 18:44:54 -0300

    25 set 2023

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct User {
+    name: String,
+    email: String
+}
+
+fn build_user(name: String, email: String) -> User {
+    User {
+        name,
+        email
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user: User = build_user(String::from("Danilo A. Silva"), String::from("dan@mail"));
+
+    print!("{} : {}\n", user.name, user.email);
 }
